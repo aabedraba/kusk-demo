80200835c132d0cbc24d712425cae6dade7d6357
Abdallah Abedraba <aabedraba@gmail.com>
2022-07-06 11:31:17 +0200

    move hello-world app to deno

---
+++ hello-world-container/index.ts
@@ -0,0 +1,17 @@
+import { Application, Router } from "https://deno.land/x/oak@v10.6.0/mod.ts";
+
+const app = new Application();
+
+const router = new Router();
+
+router.get("/hello", (ctx) => {
+  return (ctx.response.body = "Hello from an implemented service!");
+});
+
+router.post("/validated", async (ctx) => {
+  const requestBody = await ctx.request.body().value;
+  return (ctx.response.body = `Hello ${requestBody.name}!`);
+});
+
+app.use(router.routes());
+await app.listen({ port: 8080 });
