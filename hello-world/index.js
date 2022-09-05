97601e08f753e9cf0a4bd58d6f7cd9ecb5686cc7
Abdallah Abedraba <aabedraba@gmail.com>
2022-09-06 00:44:25 +0400

    add root path to example

--- hello-world/index.js
+++ hello-world/index.js
@@ -3,19 +3,32 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-app.get("/hello", function (_, res) {
+app.get("/", (req, res) => {
+  const hostname = process.env.HOSTNAME || null;
+  if (hostname === null) {
+    return res.send(
+      "No hostname information, perhaps the application is not running in Kubernetes?"
+    );
+  }
+  return res.send({
+    message: "Hello from Pod " + hostname
+  })
+});
+
+app.get("/hello", (_, res) => {
   res.send({
     message: "Hello from an implemented service!",
   });
 });
 
-app.post("/validated", function (req, res) {
+app.post("/validated", (req, res) => {
   res.send({
     message: "Hello " + req.body.name + "!",
   });
 });
 
 const port = process.env.PORT || "8080";
-app.listen(port, function () {
+
+app.listen(port, () => {
   console.log("Listening on http://localhost:" + port);
 });
